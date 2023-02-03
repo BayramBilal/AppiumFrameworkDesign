61777e713b5c56fc4f815df67e6835ddd3d96ce0
Bilal <105069025+BayramBilal@users.noreply.github.com>
2023-02-03 17:15:41 +0300

    Initial commit

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,87 @@
+import com.google.common.collect.ImmutableMap;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+import org.testng.annotations.BeforeClass;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+public class BaseTest {
+    public AndroidDriver driver;
+    public AppiumDriverLocalService serviceBuilder;
+@BeforeClass
+    public void ConfigureAppium() throws MalformedURLException {
+
+
+
+        serviceBuilder = new AppiumServiceBuilder().withAppiumJS(new File("C:\\Users\\Selim\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js"))
+                .withIPAddress("127.0.0.1").usingPort(4723).build();
+        serviceBuilder.start();
+
+        UiAutomator2Options options=  new UiAutomator2Options();
+        options.setDeviceName("Phone1");
+//        options.setDeviceName("Android Device");
+
+        options.setChromedriverExecutable("C:\\Users\\Selim\\chromedriver_win32\\chromedriver.exe");
+        options.setApp("C:\\Users\\Selim\\IdeaProjects\\AppiumBilal\\src\\test\\java\\resources\\ApiDemos-debug.apk");
+//        options.setApp("C:\\Users\\Selim\\IdeaProjects\\AppiumBilal\\src\\test\\java\\resources\\General-store.apk");
+        // Actual automation
+        // id, xpath, accessibiltyId, className, androidUIAutomator
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+}
+
+public void longPressAction(WebElement elementPeopleNames){
+
+    ((JavascriptExecutor)driver).executeScript("mobile: longClickGesture",
+            ImmutableMap.of("elementId", ((RemoteWebElement)elementPeopleNames).getId(), "duration", 2000));
+}
+
+
+public void scrollToEndAction(){
+
+    boolean canScrollMore;
+    do {
+        canScrollMore = (Boolean) ((JavascriptExecutor) driver).executeScript("mobile: scrollGesture", ImmutableMap.of(
+                "left", 100, "top", 100, "width", 200, "height", 200,
+                "direction", "down",
+                "percent", 3.0
+        ));
+    }while(canScrollMore);
+
+
+}
+public void swipeAction (WebElement ele, String direction){
+    ((JavascriptExecutor) driver).executeScript("mobile: swipeGesture", ImmutableMap.of(
+            "elementId", ((RemoteWebElement)ele).getId(),
+            "direction", "left",
+            "percent", 0.75
+    ));
+
+
+}
+public Double getFormattedAmount(String amount){
+
+    Double price = Double.parseDouble(amount.substring(1));
+    return price;
+}
+
+
+//
+//@AfterClass
+//public void tearDown(){
+//
+//
+//    driver.quit();
+//    serviceBuilder.stop();
+//
+//}
+}
---
+++ src/test/java/eCommerce_tc_4_Hybrid.java
@@ -0,0 +1,88 @@
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Set;
+
+public class eCommerce_tc_4_Hybrid extends BaseTest {
+
+
+    @Test
+    public void FillForm() throws InterruptedException {
+
+    driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("Eva Peron");
+    driver.hideKeyboard();
+    driver.findElement(By.xpath("//android.widget.RadioButton[@text='Female']")).click();
+    driver.findElement(By.id("android:id/text1")).click();
+    driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Argentina\"));"));
+    driver.findElement(By.xpath("//android.widget.TextView[@text='Argentina']")).click();
+
+    driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
+
+    driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Jordan 6 Rings\"));"));
+
+//    driver.findElements(By.xpath("//android.widget.TextView[@text='ADD TO CHART']")).get(0).click();
+//    driver.findElements(By.xpath("//android.widget.TextView[@text='ADD TO CHART']")).get(0).click();
+
+        int productCount = driver.findElements(By.id("com.androidsample.generalstore:id/productName")).size();
+        for (int i = 0; i < productCount; i++) {
+            String productName = driver.findElements(By.id("com.androidsample.generalstore:id/productName")).get(i).getText();
+            if(productName.equalsIgnoreCase("Jordan 6 Rings")){
+                driver.findElements(By.id("com.androidsample.generalstore:id/productAddCart")).get(i).click();
+            }
+            if(productName.equalsIgnoreCase("Jordan Lift Off")){
+                driver.findElements(By.id("com.androidsample.generalstore:id/productAddCart")).get(i).click();
+            }
+        }
+
+    driver.findElement(By.id("com.androidsample.generalstore:id/appbar_btn_cart")).click();
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+    wait.until(ExpectedConditions.attributeContains(driver.findElement(By.id("com.androidsample.generalstore:id/toolbar_title")), "text", "Cart"));
+    List<WebElement> productPrices = driver.findElements(By.id("com.androidsample.generalstore:id/productPrice"));
+    int count = productPrices.size();
+    double totalSum = 0;
+
+        for (int i = 0; i < count; i++) {
+           String amountString = productPrices.get(i).getText();
+           //$160.97
+           Double price = getFormattedAmount(amountString);
+           totalSum = totalSum + price;   //160.97 + 120 = 280.97
+        }
+        String displaySum = driver.findElement(By.id("com.androidsample.generalstore:id/totalAmountLbl")).getText();
+        Double displatFormattedSum = getFormattedAmount(displaySum);
+        System.out.println( totalSum+" = " + displatFormattedSum);
+        Assert.assertEquals(totalSum, displatFormattedSum);
+
+        longPressAction(driver.findElement(By.id("com.androidsample.generalstore:id/termsButton")));
+        driver.findElement(By.id("android:id/button1")).click();
+        driver.findElement(AppiumBy.className("android.widget.CheckBox")).click();
+        driver.findElement(By.id("com.androidsample.generalstore:id/btnProceed")).click();
+        Thread.sleep(6000);
+
+        //Hybrid app -->Google page -->
+
+    Set<String> contexts = driver.getContextHandles();
+        for (String contextName : contexts) {
+            System.out.println(contextName);
+        }
+        driver.context("WEBVIEW_com.androidsample.generalstore"); // need chrome driver
+        driver.findElement(By.name("q")).sendKeys("rahul shetty academy", Keys.ENTER);
+        driver.pressKey(new KeyEvent(AndroidKey.BACK));
+        driver.context("NATIVE_APP");
+
+
+
+
+
+
+    }
+}
